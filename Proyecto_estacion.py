d062fb5520f4ebd61ee2252a6f429463915b6067
liliird <142356931+liliird@users.noreply.github.com>
2023-10-19 22:27:47 -0600

    Proyecto_estacion.py

--- Proyecto_estacion.py
+++ Proyecto_estacion.py
@@ -1,33 +1,49 @@
 import requests
 from datetime import datetime
+from statistics import mean, median, mode
 
 def menu():
-    print("1. Conocer los datos meteorológicos")
+    print("\n1. Conocer los datos meteorológicos")
     print("2. Mostrar ciudades consultadas")
     print("3. Mostrar detalles de una ciudad consultada")
     print("4. Salir")
+    print("--------------------------------------------------------------------------------")
 
-def getInfo(url):
+def get_info(url):
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
     else:
         return "Error"
 
-def printInfoCoordenadas(info):
+def print_info_coordenadas(info):
     print("Longitud:", info.get("lon"))
     print("Latitud:", info.get("lat"))
 
-def printInfoClima(info):
-    print("Temperatura en grados Celsius:", info.get("temp"), "°C")
-    print("Temperatura en grados Fahrenheit:", info.get("temp") * (9 / 5) + 32, "°F")
-    print("Temperatura en grados Kelvin:", info.get("temp") + 273.15, "K")
+def print_info_clima(info):
+    temp = info.get("temp")
+    temp_min = info.get("temp_min")
+    temp_max = info.get("temp_max")
+    #Uso de operadores aritméticos para conocer la temperatura en diferentes unidades de medida
+    print("Temperatura en grados Celsius:", temp, "°C")
+    print("Temperatura en grados Fahrenheit:", temp * (9 / 5) + 32, "°F")
+    print("Temperatura en grados Kelvin:", temp + 273.15, "K")
     print("Humedad:", info.get("humidity"), "%")
-    print("Temperatura Mínima:", info.get("temp_min"), "°C")
-    print("Temperatura Máxima:", info.get("temp_max"), "°C")
-    print("Promedio de temperatura:", (info.get("temp_min") + info.get("temp_max")) / 2, "°C")
+    print("Temperatura Mínima:", temp_min, "°C")
+    print("Temperatura Máxima:", temp_max, "°C")
+    
+    # Calculamos la media, mediana y moda de las temperaturas con ayuda de la librería statistics 
+    temperaturas = [temp, temp_min, temp_max]
+    
+    print("Media de temperatura:", mean(temperaturas), "°C")
+    print("Mediana de temperatura:", median(temperaturas), "°C")
+    
+    try:
+        print("Moda de temperatura:", mode(temperaturas), "°C")
+    except:
+        print("No hay moda para las temperaturas")
 
-def printViento(info):
+def print_viento(info):
     print("Viento: ", info.get("speed"))
 
 def main():
@@ -37,21 +53,24 @@ def main():
     while continua:
         menu()
         opcion = int(input("Introduce una opción: "))
+        print("--------------------------------------------------------------------------------")
         if opcion == 1:
             ciudad = input("Introduce la ciudad para conocer su información meteorológica: ")
+            #URL de la API obtenida de OpenWeatherMap
             url = "http://api.openweathermap.org/data/2.5/weather?q=" + ciudad + "&mode=json&units=metric&lang=es&appid=9fc4e2b08423fd7dbcf8a27d0e755035"
-            response = getInfo(url)
+            response = get_info(url)
             if response != "Error":
                 print("Ciudad:", response.get("name"))
-                printInfoCoordenadas(response.get("coord"))
-                printInfoClima(response.get("main"))
-                printViento(response.get("wind"))
+                print_info_coordenadas(response.get("coord"))
+                print_info_clima(response.get("main"))
+                print_viento(response.get("wind"))
                 # Información de la ciudad consultada en la lista como un diccionario
                 ciudad_info = {
                     "name": response.get("name"),
                     "coord": response.get("coord"),
                     "main": response.get("main"),
-                    "wind": response.get("wind")
+                    "wind": response.get("wind"),
+                    "historial": []  # Lista anidada para almacenar el historial de consultas
                 }
                 ciudades_consultadas.append(ciudad_info)
             else:
@@ -66,9 +85,9 @@ def main():
                 cd_selec= ciudades_consultadas[indice]
                 print("Detalles de la ciudad seleccionada:")
                 print("Ciudad:", cd_selec["name"])
-                printInfoCoordenadas(cd_selec["coord"])
-                printInfoClima(cd_selec["main"])
-                printViento(cd_selec["wind"])
+                print_info_coordenadas(cd_selec["coord"])
+                print_info_clima(cd_selec["main"])
+                print_viento(cd_selec["wind"])
             else:
                 print("Índice de ciudad no válido.")
         elif opcion == 4:
